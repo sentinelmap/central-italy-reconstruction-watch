7e699052123845f76df95b4f6272cc132ae7154a
Carlo Palermo <carlo.palermo@hdati.com>
2016-11-24 15:50:02 +0100

    Update data, add relations

--- main.js
+++ main.js
@@ -8,10 +8,13 @@ var osmAttr = ' | © <a href="https://openstreetmap.org/copyright">OSM</a>' +
 
 var Attr = satAttr + osmAttr ;
 
+// https://mapzen.com/documentation/vector-tiles/layers/#boundaries-properties-optional
+// osm_relation: true, which can also be deduced from negative id values.
+
 var tit = 'Earthquake damage: </br>'
-var OSM0 = '</br> <a href="http://www.openstreetmap.org/way/'
+var OSM0 = '</br> <a href="http://www.openstreetmap.org/'
 var OSM1 = '/" target="_blank">View on OSM</a> </br>'
-var iD0 = '<a href="http://www.openstreetmap.org/edit?way='
+var iD0 = '<a href="http://www.openstreetmap.org/edit?'
 var iD1 = '" target="_blank">Edit with iD</a>'
 
 var scLayer = Tangram.leafletLayer({
@@ -26,17 +29,23 @@ var scLayer = Tangram.leafletLayer({
 	    }
 	},
 	click: function(selection) {
-	    //console.log(selection);
+	    // console.log(selection);
 	    if (selection.feature != null & map.getZoom() >= 17) {
-		fid = String(selection.feature.properties.id);
 		latlng = selection.leaflet_event.latlng;
+		// info elements
 		bb = String(selection.feature.layers);
 		dam = String(bb.split('buildings:', 2)[1]);
 		dam = dam.replace(',', '');
 		dam = dam.replace('_damage', '');
+		// link elements
+		fid = selection.feature.properties.id;
+		if(fid >= 0) { type= 'way' } else { type='relation' }
+		str_id = String( Math.abs(fid) )
 		var popup = L.popup({ className: 'custom' })
 		    .setLatLng(latlng)
-		    .setContent(tit + dam + OSM0 + fid + OSM1 + iD0 + fid + iD1)
+		    .setContent(tit + dam +
+				OSM0 + type + '/' + str_id + OSM1 +
+				iD0 + type + '=' + str_id + iD1)
 		    .openOn(map);
 	    } //end of if
 	} //end of click
